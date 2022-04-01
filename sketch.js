7d8b3d3115b9ff08ff46dcd8fa765e19e77ead23
nickthorn402 <98910010+nickthorn402@users.noreply.github.com>
2022-04-01 12:40:33 -0400

    new

--- sketch.js
+++ sketch.js
@@ -10,6 +10,10 @@ let rx = -50
 let ry = 50
 let rs = 50
 
+let gx = -50
+let gy = 50
+let gs = 50
+
 let speed = 5
 let score = 0
 let menuVar = 0
@@ -17,6 +21,8 @@ let menuVar = 0
 let hitRadius = 45
 let collision = false
 let spawn = 2
+let spawn2 = 2
+
 
 function setup() {
   createCanvas(300, 400);
@@ -34,6 +40,10 @@ rx = -50
 ry = 50
 rs = 50
 
+gx = -50
+gy = 50
+gs = 50
+
 speed = 5
 score = 0
 menuVar = 0
@@ -41,6 +51,7 @@ menuVar = 0
 hitRadius = 45
 collision = false
 spawn = 2
+spawn2 = 2
 
 }
 
@@ -75,6 +86,7 @@ function menu() {
   background(200)
   fill(0)
   text('Menu', 10, 15)
+  text('Score = ' + score, 100, 15)
 
   if(menuVar == 0) {
     text('Press Space To Play', 100, 200)
@@ -97,11 +109,9 @@ function game() {
   menuVar = 1
   text('Game', 10, 15 )
   text('Score = ' + score, 100, 15)
-  text('Debug = ' + spawn, 200, 15)
+  text('Debug = ' + spawn2, 200, 15)
   fill(0, 255, 0)
   rect(playerx, playery, 60)
-
-
 
   if (playerx < 75) {
     playerx = 75;
@@ -110,17 +120,39 @@ function game() {
     playerx = 225;
   }
 
-  
   fill(255, 0, 0)
   circle(rx, ry, rs)
+  circle(gx, gy, gs)
+  if (score <= 5) {
+    gx = -100
+  } else{
+    if (spawn2 == 1) {
+      gx = 75
+    }
+  
+    if (spawn2 == 2) {
+      gx = 150
+    }
+  
+    if (spawn2 == 3) {
+      gx = 225
+    }
+  }
 
   if (ry < 425) {
     ry += speed;
   } else{
     ry = -25
     spawn = int(random(1,4))
-    speed += 0.1
+    speed += 0.2
     score += 1
+  }
+
+  if (gy < 425) {
+    gy += speed;
+  } else{
+    gy = -25
+    spawn2 = int(random(1,4))
   }
 
   if (spawn == 1) {
@@ -135,12 +167,19 @@ function game() {
     rx = 225
   }
 
+  
+
   if (abs(playerx - rx) < hitRadius && abs(playery - ry) < hitRadius) {
     collision = true
   }
 
+  if (abs(playerx - gx) < hitRadius && abs(playery - gy) < hitRadius) {
+    collision = true
+  }
+
   if (collision == true) {
-  menuVar = 2
+    menuVar = 2
+    scene *= -1
     menu()
   }
 }
